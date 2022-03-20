d0323573a42312e756419cb946f1633bce9c71ce
Tzur <zorchar@gmail.com>
2022-03-20 15:43:56 +0200

    search bar design remains and now on to logic

---
+++ main.js
@@ -0,0 +1,99 @@
+const settlementsData = [
+    {
+        settlement: 'Tel-Aviv',
+        firstShotPercentage: 95,
+        secondShotPercentage: 94,
+        thirdShotPercentage: 93,
+        activeSickPerTenThousand: 88.8,
+        dailyCalculatedGrade: 9.9,
+        newSickPerTenThousand: 126.3,
+        positiveTestsPercantage: 20,
+        verifiedChangeRatio: 84,
+        activeSick: 2132
+    },
+    {
+        settlement: 'Haifa',
+        firstShotPercentage: 91,
+        secondShotPercentage: 90,
+        thirdShotPercentage: 88,
+        activeSickPerTenThousand: 85.8,
+        dailyCalculatedGrade: 9.3,
+        newSickPerTenThousand: 226.3,
+        positiveTestsPercantage: 15,
+        verifiedChangeRatio: 80,
+        activeSick: 1001
+    },
+    {
+        settlement: 'Jerusalem',
+        firstShotPercentage: 88,
+        secondShotPercentage: 87,
+        thirdShotPercentage: 86,
+        activeSickPerTenThousand: 185.8,
+        dailyCalculatedGrade: 8.2,
+        newSickPerTenThousand: 123.5,
+        positiveTestsPercantage: 26,
+        verifiedChangeRatio: 35,
+        activeSick: 1751
+    },
+    {
+        settlement: 'Kefar-Kama',
+        firstShotPercentage: 73.79,
+        secondShotPercentage: 69.94,
+        thirdShotPercentage: 55.12,
+        activeSickPerTenThousand: 58.9,
+        dailyCalculatedGrade: 9.9,
+        newSickPerTenThousand: 79.5,
+        positiveTestsPercantage: 27,
+        verifiedChangeRatio: 80,
+        activeSick: 20
+    },
+    {
+        settlement: 'Matan',
+        firstShotPercentage: 84.74,
+        secondShotPercentage: 81.98,
+        thirdShotPercentage: 66.42,
+        activeSickPerTenThousand: 148.8,
+        dailyCalculatedGrade: 9.9,
+        newSickPerTenThousand: 165.3,
+        positiveTestsPercantage: 23,
+        verifiedChangeRatio: 47,
+        activeSick: 54
+    },
+    {
+        settlement: 'Ashdod',
+        firstShotPercentage: 82.73,
+        secondShotPercentage: 22.91,
+        thirdShotPercentage: 46.42,
+        activeSickPerTenThousand: 248.8,
+        dailyCalculatedGrade: 6.9,
+        newSickPerTenThousand: 865.3,
+        positiveTestsPercantage: 29,
+        verifiedChangeRatio: 65,
+        activeSick: 657
+    },
+    {
+        settlement: 'Ashkelon',
+        firstShotPercentage: 24.74,
+        secondShotPercentage: 21.98,
+        thirdShotPercentage: 16.42,
+        activeSickPerTenThousand: 765.8,
+        dailyCalculatedGrade: 8.4,
+        newSickPerTenThousand: 65.3,
+        positiveTestsPercantage: 83,
+        verifiedChangeRatio: 87,
+        activeSick: 876
+    },
+    {
+        settlement: 'Furaidis',
+        firstShotPercentage: 94.74,
+        secondShotPercentage: 91.98,
+        thirdShotPercentage: 86.42,
+        activeSickPerTenThousand: 48.8,
+        dailyCalculatedGrade: 10,
+        newSickPerTenThousand: 5.3,
+        positiveTestsPercantage: 3,
+        verifiedChangeRatio: 4,
+        activeSick: 5
+    },
+]
+
